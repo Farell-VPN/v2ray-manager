07d88c5d8da9a8f63d8e2eb1278e1a238330e6f8
Farell Aditya Ardian <farell@rerechanstore.eu.org>
2025-02-28 12:12:21 +0700

    Create credit.sh
    add credit

---
+++ menu/credit.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo -e "
+Terimakasih telah menggunakan script kami, disini kami akan mencantumkan credit dari beberapa orang yang berkontribusi atas script ini.
+
+- Farell Aditya
+- Rerechan02 / Dinda Putri
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"
