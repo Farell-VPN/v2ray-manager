b5c95ecd17967015157cb63b589690152ad01ddb
Farell Aditya Ardian <farell@rerechanstore.eu.org>
2025-03-01 10:09:48 +0700

    Update v2ray.sh

--- installer/v2ray.sh
+++ installer/v2ray.sh
@@ -18,3 +18,24 @@ sed -i 's|User=nobody|User=root|g' v2ray.service
 cd
 
 clear
+
+# Setup Json
+wget -O /usr/local/etc/v2ray/config.json "https://raw.githubusercontent.com/Farell-VPN/v2ray-manager/refs/heads/main/json/config.json"
+
+# Permision Log
+rm -fr /var/log/v2ray
+mkdir -p /var/log/v2ray
+touch /var/log/v2ray/access.log
+touch /var/log/v2ray/error.log
+chmod 755 /var/log/v2ray/access.log
+chown root:root /var/log/v2ray/access.log
+chmod 755 /var/log/v2ray/error.log
+chown root:root /var/log/v2ray/error.log
+
+# Start Service
+systemctl daemon-reload
+systemctl enable v2ray
+systemctl start v2ray
+systemctl restart v2ray
+
+clear
