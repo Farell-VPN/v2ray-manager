4b8698ef9683f163bcb3c401d27ed11d2345b96e
Farell Aditya Ardian <farell@rerechanstore.eu.org>
2025-03-01 09:50:15 +0700

    Create v2ray.sh

---
+++ installer/v2ray.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+
+echo -e "
++-+-+-+-+-+-+-+-+-+-+-+-+
+|F|A|R|E|L| |A|D|I|T|Y|A|
++-+-+-+-+-+-+-+-+-+-+-+-+
+"
+
+# Setup Core V2rayfly
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
